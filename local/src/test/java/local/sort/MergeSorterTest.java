d5e26d51956e27d7801a7b0281dc9b4e259b9c25
xiaoshuang.cui <humanbeingxx@sina.com>
2018-01-23 02:08:56 +0800

    add first merge sort

---
+++ local/src/test/java/local/sort/MergeSorterTest.java
@@ -0,0 +1,28 @@
+package local.sort;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSorterTest {
+    @Test
+    public void testSort() throws Exception {
+        List<Integer> data = Lists.newArrayList(2, 1, 7, 5, 4, 0, 8);
+        MergeSorter<Integer> sorter = new MergeSorter<>(data);
+        sorter.sort();
+        System.out.println(sorter.getOriginData());
+    }
+
+    @Test
+    public void testMerge() throws Exception {
+        List<Integer> data = Lists.newArrayList(5, 4, 3, 2, 1);
+        MergeSorter<Integer> sorter = new MergeSorter<>(data);
+        ArrayList<Integer> to = Lists.newArrayList(new Integer[6]);
+        ArrayList<Integer> result = Lists.newArrayList(1, 3, 5, 2, 4, 6);
+        sorter.merge(result, 0, 2, 5, to);
+        System.out.println(to);
+        System.out.println(result);
+    }
+}
