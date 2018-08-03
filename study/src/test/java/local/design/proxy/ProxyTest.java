a06464d2fdf311a4da12ecefa6dcab2bfac27280
xiaoshuang.cui <humanbeingxx@sina.com>
2018-08-03 11:53:53 +0800

    update

--- study/src/test/java/local/design/proxy/ProxyTest.java
+++ study/src/test/java/local/design/proxy/ProxyTest.java
@@ -8,7 +8,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
- * Copyright © 2018 QUNAR.COM. All rights reserved.
  *
  * @author xiaoshuang.cui
  * @date 2018/7/24 下午4:41
---
+++ study/src/test/java/local/other/ModCountTest.java
@@ -0,0 +1,33 @@
+package local.other;
+
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author xiaoshuang.cui
+ * @date 2018/8/3 上午11:49
+ **/
+public class ModCountTest {
+
+    @Test
+    public void test() throws NoSuchFieldException, IllegalAccessException {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add("a");
+            System.out.println(getArray(list).length);
+        }
+        System.out.println("add end");
+
+        list.clear();
+        System.out.println(getArray(list).length);
+    }
+
+    private Object[] getArray(ArrayList list) throws NoSuchFieldException, IllegalAccessException {
+        final Field array = ArrayList.class.getDeclaredField("elementData");
+        array.setAccessible(true);
+        return (Object[]) array.get(list);
+    }
+}
