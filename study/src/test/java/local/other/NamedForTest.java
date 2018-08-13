112586ac63e2fa447ae508b31837d02e939453c5
xiaoshuang.cui <humanbeingxx@sina.com>
2018-08-13 22:29:16 +0800

    add new file

---
+++ study/src/test/java/local/other/NamedForTest.java
@@ -0,0 +1,37 @@
+package local.other;
+
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+/**
+ * Copyright © 2018 QUNAR.COM. All rights reserved.
+ *
+ * @author xiaoshuang.cui
+ * @date 2018/8/13 下午9:49
+ **/
+public class NamedForTest {
+
+    @Test
+    public void test() {
+        out:while (true) {
+            in:for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+                if (i % 3 != 0) {
+                    break out;
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testIf() {
+        a:while (true) {
+            final int anInt = new Random(System.currentTimeMillis()).nextInt(1000);
+            if (anInt / 5 == 0) {
+                System.out.println(anInt);
+                break a;
+            }
+        }
+    }
+}
