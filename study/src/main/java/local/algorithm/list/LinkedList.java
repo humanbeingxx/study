6c5856c762e6e54ef0eaa0065389155e1f08cb10
xiaoshuang.cui <humanbeingxx@sina.com>
2018-08-30 17:23:12 +0800

    update

---
+++ study/src/main/java/local/algorithm/list/LinkedList.java
@@ -0,0 +1,72 @@
+package local.algorithm.list;
+
+/**
+ * Copyright © 2018 QUNAR.COM. All rights reserved.
+ *
+ * @author xiaoshuang.cui
+ * @date 2018/8/30 下午5:02
+ **/
+public class LinkedList {
+
+    public static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(data);
+        }
+    }
+
+    private Node head;
+
+    private Node tail;
+
+    public LinkedList() {
+        head = new Node(0);
+        tail = head;
+    }
+
+    public void add(Node node) {
+        tail.next = node;
+        tail = node;
+    }
+
+    public void reverse() {
+        if (head.next == null) {
+            return;
+        }
+
+        Node newTail = head.next;
+        head.next = tail;
+        tail = newTail;
+        reverseRec(newTail, newTail.next);
+        tail.next = null;
+    }
+
+    private void reverseRec(Node current, Node next) {
+        if (current == null || next == null) {
+            return;
+        }
+        Node nnext = next.next;
+        next.next = current;
+
+        reverseRec(next, nnext);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("head->");
+        Node index = head.next;
+        while (index != null) {
+            sb.append(index.toString()).append("->");
+            index = index.next;
+        }
+
+        return sb.subSequence(0, sb.length() - 2).toString();
+    }
+}
---
+++ study/src/test/java/local/algorithm/list/LinkedListTest.java
@@ -0,0 +1,69 @@
+package local.algorithm.list;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Copyright © 2018 QUNAR.COM. All rights reserved.
+ *
+ * @author xiaoshuang.cui
+ * @date 2018/8/30 下午5:07
+ **/
+public class LinkedListTest {
+
+    @Test
+    public void testAdd() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(new LinkedList.Node(1));
+        linkedList.add(new LinkedList.Node(2));
+        linkedList.add(new LinkedList.Node(3));
+        linkedList.add(new LinkedList.Node(4));
+
+        System.out.println(linkedList);
+    }
+
+    @Test
+    public void testReverse() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(new LinkedList.Node(1));
+        linkedList.add(new LinkedList.Node(2));
+        linkedList.add(new LinkedList.Node(3));
+        linkedList.add(new LinkedList.Node(4));
+
+        linkedList.reverse();
+
+        System.out.println(linkedList);
+    }
+
+    @Test
+    public void testReverse2() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(new LinkedList.Node(1));
+
+        linkedList.reverse();
+
+        System.out.println(linkedList);
+    }
+
+    @Test
+    public void testAll() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(new LinkedList.Node(1));
+        linkedList.add(new LinkedList.Node(2));
+        linkedList.add(new LinkedList.Node(3));
+        linkedList.add(new LinkedList.Node(4));
+
+        linkedList.reverse();
+
+        linkedList.add(new LinkedList.Node(1));
+        linkedList.add(new LinkedList.Node(2));
+        linkedList.add(new LinkedList.Node(3));
+        linkedList.add(new LinkedList.Node(4));
+
+        System.out.println(linkedList);
+
+        linkedList.reverse();
+        System.out.println(linkedList);
+    }
+}
