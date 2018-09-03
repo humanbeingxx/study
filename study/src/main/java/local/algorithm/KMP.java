596cd0d69c7fd87f931c0d9e4f446b9afffec287
xiaoshuang.cui <humanbeingxx@sina.com>
2018-09-03 22:07:14 +0800

    update

---
+++ study/src/main/java/local/algorithm/KMP.java
@@ -0,0 +1,50 @@
+package local.algorithm;
+
+/**
+ * Copyright © 2018 QUNAR.COM. All rights reserved.
+ *
+ * @author xiaoshuang.cui
+ * @date 2018/9/2 下午4:54
+ **/
+public class KMP {
+
+    public static int search(String search, String from) {
+        if (search.length() < 1 || from.length() < 1) {
+            return -1;
+        }
+        int[] nexts = getNext(search);
+
+        int indexOfSearch = 0;
+        int indexOfFrom = 0;
+
+        for (;indexOfFrom < from.length();indexOfFrom++) {
+            while (indexOfSearch > 0 && from.charAt(indexOfFrom) != search.charAt(indexOfSearch)) {
+                indexOfSearch = nexts[indexOfSearch - 1];
+            }
+            if (from.charAt(indexOfFrom) == search.charAt(indexOfSearch)) {
+                indexOfSearch++;
+            }
+            if (indexOfSearch == search.length()) {
+                return indexOfFrom - search.length() + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    protected static int[] getNext(String str) {
+        char[] p = str.toCharArray();
+        int[] next = new int[p.length];
+        next[0] = 0;
+        for (int q = 1, k = 0; q < p.length; ++q) {
+            while (k > 0 && p[q] != p[k]) {
+                k = next[k - 1];
+            }
+            if (p[q] == p[k]) {
+                k++;
+            }
+            next[q] = k;
+        }
+        return next;
+    }
+}
---
+++ study/src/test/java/local/algorithm/KMPTest.java
@@ -0,0 +1,52 @@
+package local.algorithm;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+import static org.testng.Assert.*;
+
+/**
+ * Copyright © 2018 QUNAR.COM. All rights reserved.
+ *
+ * @author xiaoshuang.cui
+ * @date 2018/9/2 下午4:56
+ **/
+public class KMPTest {
+
+    @Test
+    public void testSearch1() {
+        assertEquals(KMP.search("cb", "abcba"), 2);
+    }
+
+    @Test
+    public void testSearch2() {
+        assertEquals(KMP.search("x", "abcdefg"), -1);
+    }
+
+    @Test
+    public void testSearch3() {
+        assertEquals(KMP.search("aa", "aaaa"), 0);
+    }
+
+    @Test
+    public void testSearch4() {
+        assertEquals(KMP.search("cb", "abcba"), 2);
+    }
+
+    @Test
+    public void testSearch5() {
+        assertEquals(KMP.search("ABCDABD", "BBCABCDABABCDABCDABDE"), 13);
+    }
+
+    @Test
+    public void testSearch6() {
+        assertEquals(KMP.search("abcdef","abcdeg, abcdeh, abcdef!这个会匹配2次"), 16);
+    }
+
+    @Test
+    public void testGetNext() {
+        int[] next = KMP.getNext("aabcaaaba");
+        System.out.println(Arrays.toString(next));
+    }
+}
