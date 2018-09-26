767e0159a0d12cade0eaf9ab131810e3f8da114f
xiaoshuang.cui <humanbeingxx@sina.com>
2018-09-26 17:05:20 +0800

    update

---
+++ study/src/main/java/local/sort/RawMergeSort.java
@@ -0,0 +1,56 @@
+package local.sort;
+
+/**
+ * Copyright © 2018 QUNAR.COM. All rights reserved.
+ *
+ * @author xiaoshuang.cui
+ * @date 2018/9/26 下午4:11
+ **/
+public class RawMergeSort {
+
+    public static void sort(int[] data) {
+        if (data.length <= 1) {
+            return;
+        }
+        int[] temp = new int[data.length];
+
+        sortRec(data, 0, data.length - 1, temp);
+    }
+
+    private static void sortRec(int[] data, int begin, int end, int[] temp) {
+        if (begin >= end) {
+            return;
+        }
+        sortRec(data, begin, (begin + end) / 2, temp);
+        sortRec(data, (begin + end) / 2 + 1, end, temp);
+
+        merge(data, begin, (begin + end) / 2, end, temp);
+    }
+
+    private static void merge(int[] data, int begin, int mid, int end, int[] temp) {
+        int index1 = begin;
+        int index2 = mid + 1;
+        int indexForTemp = begin;
+
+        while (index1 <= mid && index2 <= end) {
+            if (data[index1] > data[index2]) {
+                temp[indexForTemp++] = data[index2++];
+            } else if (data[index1] < data[index2]) {
+                temp[indexForTemp++] = data[index1++];
+            } else {
+                temp[indexForTemp++] = data[index1++];
+                temp[indexForTemp++] = data[index2++];
+            }
+        }
+
+        while (index1 <= mid) {
+            temp[indexForTemp++] = data[index1++];
+        }
+
+        while (index2 <= end) {
+            temp[indexForTemp++] = data[index2++];
+        }
+
+        System.arraycopy(temp, begin, data, begin, end - begin + 1);
+    }
+}
---
+++ study/src/test/java/local/sort/RawMergeSortTest.java
@@ -0,0 +1,70 @@
+package local.sort;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+/**
+ * Copyright © 2018 QUNAR.COM. All rights reserved.
+ *
+ * @author xiaoshuang.cui
+ * @date 2018/9/26 下午4:42
+ **/
+public class RawMergeSortTest {
+
+    @Test
+    public void testSort() {
+
+        int[] data = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        RawMergeSort.sort(data);
+
+        System.out.println(Arrays.toString(data));
+        Assert.assertEquals(data, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+    }
+
+    @Test
+    public void testSortEmpty() {
+
+        int[] data = {};
+
+        RawMergeSort.sort(data);
+
+        System.out.println(Arrays.toString(data));
+        Assert.assertEquals(data, new int[]{});
+    }
+
+    @Test
+    public void testSortSame() {
+
+        int[] data = {1, 1, 1, 1, 1, 1, 1};
+
+        RawMergeSort.sort(data);
+
+        System.out.println(Arrays.toString(data));
+        Assert.assertEquals(data, new int[]{1, 1, 1, 1, 1, 1, 1});
+    }
+
+    @Test
+    public void testSortOne() {
+
+        int[] data = {1};
+
+        RawMergeSort.sort(data);
+
+        System.out.println(Arrays.toString(data));
+        Assert.assertEquals(data, new int[]{1});
+    }
+
+    @Test
+    public void testSortTwo() {
+
+        int[] data = {3, 1};
+
+        RawMergeSort.sort(data);
+
+        System.out.println(Arrays.toString(data));
+        Assert.assertEquals(data, new int[]{1, 3});
+    }
+}
