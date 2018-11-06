cc5653ac796337200d1b0f34781ddd1b036fbfd6
xiaoshuang.cui <xiaoshuang.cui@qunar.com>
2018-11-06 23:12:16 +0800

    update

--- springboot2/src/test/java/priv/cxs/springboot2/Springboot2ApplicationTests.java
+++ springboot2/src/test/java/priv/cxs/springboot2/Springboot2ApplicationTests.java
@@ -3,14 +3,21 @@ package priv.cxs.springboot2;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot2ApplicationTests {
 
+	@Resource
+	private JdbcTemplate jdbcTemplate;
+
 	@Test
 	public void contextLoads() {
+		System.out.println(jdbcTemplate.getClass());
 	}
 
 }
