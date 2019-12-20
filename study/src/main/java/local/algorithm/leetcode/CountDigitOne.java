f73d8eb277597f76d89762d03a7b6a6dfa1dc863
humanbeingxx <humanbeingxx@sina.com>
2019-12-20 23:05:42 +0800

    update

--- study/src/main/java/local/algorithm/leetcode/CountDigitOne.java
+++ study/src/main/java/local/algorithm/leetcode/CountDigitOne.java
@@ -15,30 +15,54 @@ public class CountDigitOne {
 
     {
         oneCount.put(1, 1);
-        int i = 10;
+        oneCount.put(10, 2);
+        int i = 100;
         for (; i < Integer.MAX_VALUE / 10; i *= 10) {
-            oneCount.put(i, oneCount.get(i / 10) * 10 + i);
+            oneCount.put(i, (oneCount.get(i / 10) - 1) * 10 + i / 10 + 1);
         }
-        oneCount.put(i, oneCount.get(i / 10) * 10 + i);
+        oneCount.put(i, (oneCount.get(i / 10) - 1) * 10 + i / 10 + 1);
     }
 
     public int countDigitOne(int n) {
-        return 0;
+        if (n <= 0) {
+            return 0;
+        }
+        if (oneCount.containsKey(n)) {
+            return oneCount.get(n);
+        }
+        int pos = 1;
+        while (n / pos > 1) {
+            pos *= 10;
+        }
+        return rec(n, pos);
     }
 
-    public int force(int from, int to) {
-        int sum = 0;
-        for (int i = from; i < to; i++) {
-            int value = i;
-            int count = 0;
-            while (value > 0) {
-                if (value % 10 == 1) {
-                    count++;
-                }
-                value /= 10;
-            }
-            sum += count;
+    int rec(int num, int pos) {
+        if (num == 0) {
+            return 0;
         }
-        return sum;
+        if (num < 10) {
+            return 1;
+        }
+        if (num < pos) {
+            return rec(num, pos / 10);
+        }
+        int kn = num / pos;
+        int left = num % pos;
+        if (kn == 1) {
+            return calc10(kn, pos) + rec(left, pos / 10) + left;
+        } else {
+            return calc10(kn, pos) + rec(left, pos / 10);
+        }
+    }
+
+    int calc10(int kn, int pos) {
+        if (pos == 0) {
+            return 0;
+        }
+        if (kn == 1) {
+            return oneCount.get(pos * kn);
+        }
+        return pos + kn * (oneCount.get(pos) - 1);
     }
 }
--- study/src/test/java/local/algorithm/leetcode/CountDigitOneTest.java
+++ study/src/test/java/local/algorithm/leetcode/CountDigitOneTest.java
@@ -10,14 +10,55 @@ import static org.testng.Assert.*;
  **/
 public class CountDigitOneTest {
 
+    CountDigitOne counter = new CountDigitOne();
+
     @Test
-    public void testForce() {
-        System.out.println(new CountDigitOne().force(0, 1));
-        System.out.println(new CountDigitOne().force(0, 10));
-        System.out.println(new CountDigitOne().force(0, 100));
-        System.out.println(new CountDigitOne().force(0, 1000));
-        System.out.println(new CountDigitOne().force(0, 10000));
-        System.out.println(new CountDigitOne().force(100, 200));
-        System.out.println(new CountDigitOne().force(200, 300));
+    public void testCountDigitOne1() {
+        int result = counter.countDigitOne(1);
+        assertEquals(result, 1);
+
+        result = counter.countDigitOne(10);
+        assertEquals(result, 2);
+
+        result = counter.countDigitOne(100);
+        assertEquals(result, 21);
+
+        result = counter.countDigitOne(1000);
+        assertEquals(result, 301);
+
+        result = counter.countDigitOne(10000);
+        assertEquals(result, 4001);
+
+        System.out.println(counter.oneCount);
+    }
+
+    @Test
+    public void testCountDigitOne2() {
+        int result = counter.countDigitOne(5);
+        assertEquals(result, 1);
+
+        result = counter.countDigitOne(13);
+        assertEquals(result, 6);
+
+        result = counter.countDigitOne(20);
+        assertEquals(result, 12);
+
+        result = counter.countDigitOne(101);
+        assertEquals(result, 23);
+
+        result = counter.countDigitOne(321);
+        assertEquals(result, 173);
+
+        result = counter.countDigitOne(1011);
+        assertEquals(result, 316);
+
+        result = counter.countDigitOne(99212);
+        assertEquals(result, 49845);
+
+        result = counter.countDigitOne(1410065408);
+        assertEquals(result, 1737167499);
+
+        result = counter.countDigitOne(9999212);
+        assertEquals(result, 6999845);
     }
 }
