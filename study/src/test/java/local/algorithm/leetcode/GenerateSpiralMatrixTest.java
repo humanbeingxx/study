cb203bcaec0b5727a99b8c24bbbe0114bdbe35fc
cuixiaoshuang <cuixiaoshuang@ktzh.lvwan.com>
2020-02-25 20:12:06 +0800

    update

---
+++ study/src/test/java/local/algorithm/leetcode/GenerateSpiralMatrixTest.java
@@ -0,0 +1,38 @@
+package local.algorithm.leetcode;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+import static org.testng.Assert.*;
+
+/**
+ * @author cuixiaoshuang
+ * @date 2020-02-25
+ **/
+public class GenerateSpiralMatrixTest {
+
+    @Test
+    public void testGenerateMatrix1() {
+        int[][] result = new GenerateSpiralMatrix().generateMatrix(1);
+        for (int i = 0; i < 1; i++) {
+            System.out.println(Arrays.toString(result[i]));
+        }
+    }
+
+    @Test
+    public void testGenerateMatrix3() {
+        int[][] result = new GenerateSpiralMatrix().generateMatrix(3);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(Arrays.toString(result[i]));
+        }
+    }
+
+    @Test
+    public void testGenerateMatrix4() {
+        int[][] result = new GenerateSpiralMatrix().generateMatrix(4);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(Arrays.toString(result[i]));
+        }
+    }
+}
