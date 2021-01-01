821c1a894be4051ba1b1bcd86f588d6f1726531c
xiaoshuang.cui <xiaoshuang.cui@qunar.com>
2021-01-01 21:50:35 +0800

    for lcs with result

--- study/src/main/java/local/algorithm/leetcode/LongestCommonSubsequence.java
+++ study/src/main/java/local/algorithm/leetcode/LongestCommonSubsequence.java
@@ -1,9 +1,12 @@
 package local.algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LongestCommonSubsequence {
 
     public int longestCommonSubsequence(String text1, String text2) {
-        if (text1.length() == 0 || text2.length() ==0) {
+        if (text1.length() == 0 || text2.length() == 0) {
             return 0;
         }
         int[][] lens = new int[text1.length() + 1][text2.length() + 1];
@@ -19,5 +22,31 @@ public class LongestCommonSubsequence {
             }
         }
         return lens[text1.length()][text2.length()];
+    }
+
+    public String longestCommonSubsequenceWithResult(String text1, String text2) {
+        if (text1.length() == 0 || text2.length() == 0) {
+            return "";
+        }
+        StringBuilder[][] lens = new StringBuilder[text1.length() + 1][text2.length() + 1];
+        for (int i = 0; i <= text1.length(); i++) {
+            for (int j = 0; j <= text2.length(); j++) {
+                if (lens[i][j] == null) {
+                    lens[i][j] = new StringBuilder();
+                }
+                if (i == 0 || j == 0) {
+                    continue;
+                } else if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    lens[i][j].append(lens[i - 1][j - 1]).append(text1.charAt(i - 1));
+                } else {
+                    if (lens[i - 1][j].length() >= lens[i][j - 1].length()) {
+                        lens[i][j].append(lens[i - 1][j]);
+                    } else {
+                        lens[i][j].append(lens[i][j - 1]);
+                    }
+                }
+            }
+        }
+        return lens[text1.length()][text2.length()].toString();
     }
 }
---
+++ study/src/test/java/local/algorithm/leetcode/LongestCommonSubsequenceTest.java
@@ -0,0 +1,30 @@
+package local.algorithm.leetcode;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class LongestCommonSubsequenceTest {
+
+    @Test
+    public void testLongestCommonSubsequenceWithResult() {
+        LongestCommonSubsequence func = new LongestCommonSubsequence();
+        String text1 = "abcdefg";
+        String text2 = "acdeg";
+        int len = func.longestCommonSubsequence(text1, text2);
+        String result = func.longestCommonSubsequenceWithResult(text1, text2);
+        System.out.println(len);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testLongestCommonSubsequenceWithResult2() {
+        LongestCommonSubsequence func = new LongestCommonSubsequence();
+        String text1 = "BDCABA";
+        String text2 = "ABCBDAB";
+        int len = func.longestCommonSubsequence(text1, text2);
+        String result = func.longestCommonSubsequenceWithResult(text1, text2);
+        System.out.println(len);
+        System.out.println(result);
+    }
+}
